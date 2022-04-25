9b46650d8088ddfd1e320050e86d81a9151339da
jasonk19 <13520080@std.stei.itb.ac.id>
2022-04-26 00:42:23 +0700

    add varchar date database

---
+++ dna_pattern_matching_varchar.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dna_pattern_matching
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hasil`
+--
+
+DROP TABLE IF EXISTS `hasil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hasil` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tanggal` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `disease_name` varchar(255) NOT NULL,
+  `similarity` int NOT NULL,
+  `status` enum('True','False') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hasil`
+--
+
+LOCK TABLES `hasil` WRITE;
+/*!40000 ALTER TABLE `hasil` DISABLE KEYS */;
+INSERT INTO `hasil` VALUES (1,'20 April 2022','Lorem ipsum','Tidur',85,'True'),(2,'22 April 2022','Lorem ipsum','Tidur',100,'True'),(3,'26 April 2022','Bangun','Pusing',100,'True');
+/*!40000 ALTER TABLE `hasil` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `penyakit`
+--
+
+DROP TABLE IF EXISTS `penyakit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `penyakit` (
+  `nama_penyakit` varchar(50) NOT NULL,
+  `seq_dna` varchar(100) NOT NULL,
+  PRIMARY KEY (`nama_penyakit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `penyakit`
+--
+
+LOCK TABLES `penyakit` WRITE;
+/*!40000 ALTER TABLE `penyakit` DISABLE KEYS */;
+INSERT INTO `penyakit` VALUES ('Berdiri','ACGATCGATCGACTTGACGATCGTAGCTAGCTGAG'),('Capek','ACGATCGATCGACTTGACGATCGTAGCTAGCTGAG'),('Duduk','ACGATCGATCGACTTGACGATCGTAGCTAGCTGAG'),('Mules','ACTGATCGATCGATCGGATC'),('Pusing','ACTTGACGATCGTAGCTAGCTGAG'),('Tidur','ACTTACTGATCGATCGTGTACGTG');
+/*!40000 ALTER TABLE `penyakit` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-26  0:41:58
