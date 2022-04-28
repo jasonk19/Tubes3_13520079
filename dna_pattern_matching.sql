0b0dd310c5e1d51571f11ea97b2ea2b834c3fb4e
jasonk19 <13520080@std.stei.itb.ac.id>
2022-04-28 19:38:42 +0700

    delete varchar date data edit seq_dna to type TEXT

--- dna_pattern_matching.sql
+++ dna_pattern_matching.sql
@@ -1,13 +1,13 @@
--- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: dna_pattern_matching
 -- ------------------------------------------------------
--- Server version	10.4.22-MariaDB
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,29 +16,21 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `dna_pattern_matching`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `dna_pattern_matching` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-
-USE `dna_pattern_matching`;
-
---
 -- Table structure for table `hasil`
 --
 
 DROP TABLE IF EXISTS `hasil`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `hasil` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `tanggal` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `disease_name` varchar(255) NOT NULL,
-  `similarity` int(11) NOT NULL,
+  `similarity` int NOT NULL,
   `status` enum('True','False') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -57,12 +49,12 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `penyakit`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `penyakit` (
   `nama_penyakit` varchar(50) NOT NULL,
-  `seq_dna` varchar(100) NOT NULL,
+  `seq_dna` text NOT NULL,
   PRIMARY KEY (`nama_penyakit`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-26  8:55:35
+-- Dump completed on 2022-04-28 19:37:58
