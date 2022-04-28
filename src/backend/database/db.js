48bd707b8643f0cba862d8111a0ef3fb4a6b1576
jasonk19 <13520080@std.stei.itb.ac.id>
2022-04-28 20:06:42 +0700

    add dotenv in backend

--- src/backend/database/db.js
+++ src/backend/database/db.js
@@ -1,8 +1,11 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv").config();
+
+const sql_password = process.env.SQL_PASS;
 
 module.exports = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: sql_password,
   database: "dna_pattern_matching"
 })
